87354682d915a1466e5a0853375961ea5e2160cc
RubiniSrini <srubinirubini17@gmail.com>
2023-01-02 20:18:12 +0530

    Code updated

--- src/com/git/GitClass.java
+++ src/com/git/GitClass.java
@@ -1,8 +1,9 @@
 package com.git;
 
 public class GitClass {
-	private void method1() {
-		System.out.println(1);
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 
 }
